1432f47fc14fb148ef034ffd2afc14fd8af37268
gulnazgentle-kazan <gulnazvp@mail.ru>
2023-01-15 22:38:49 +0300

    did and added exercise 27

---
+++ ExerciseTwentySeven/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 27: Напишите программу, 
+которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSum(int a)
+{
+    int x = a;
+    int sum = 0;
+    int remainder = 0;
+    while (x > 0)
+    {
+        remainder = x % 10;
+        sum = sum + remainder;
+        x = x / 10;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число: ");
+if (number > 0)
+{
+    int summa = GetSum(number);
+    Console.WriteLine(summa);
+}
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
